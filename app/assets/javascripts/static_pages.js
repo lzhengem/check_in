be624ccc0e7e5520e0aaf512e9e50f46e81b7a1e
Lena Zheng <lzhengem@gmail.com>
2017-12-29 06:44:55 +0000

    added initGeo and initMap into geocoder.js

--- app/assets/javascripts/static_pages.js
+++
@@ -1,5 +0,0 @@
-function initGeo(){
-     window.navigator = window.navigator || {};
-     window.navigator.geolocation = window.navigator.geolocation ||
-                                    undefined;
-}
