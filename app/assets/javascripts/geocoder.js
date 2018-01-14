c13d097c6959c63625ccb57a2a1efb78b5c9ad74
Lena Zheng <lzhengem@gmail.com>
2018-01-14 02:08:15 +0000

    users can see destination on map before creating new destination

--- app/assets/javascripts/geocoder.js
+++ app/assets/javascripts/geocoder.js
@@ -147,16 +147,65 @@
         }
         
        function setupAutocomplete() {
-        // var map = new google.maps.Map(document.getElementById('map'), {
-        //   center: {lat: -33.8688, lng: 151.2195},
-        //   zoom: 13
-        // });
-        
-        // var card = document.getElementById('pac-card');
+        // set up the map to be in san francisco
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 37.773972, lng:  -122.431297},
+          zoom: 11
+        });
+
+        // set up the id address for the autocomplete
         var input = document.getElementById('address');
-        
-        // map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
         var autocomplete = new google.maps.places.Autocomplete(input);
+        // set the autocomplete bind to the map
+        autocomplete.bindTo('bounds', map);
+        
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        
+        // set up a marker
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+        
+        // when the input address chanages, change the map
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
+        
       }
     //   putting these 3 functions together for new_check_in creation so google maps api only has one callback
       function initGeo_initMap_showMap(){
