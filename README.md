e6adeaf997a20549bdf483454316bb3b537a40bd
Lena Zheng <lzhengem@gmail.com>
2018-01-14 02:16:23 +0000

    user should only be allowed 30m range

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This app uses geolocation to track the longitude and latitude of the user. 
 It uses the google maps API to geocode the address to display to the user.
 
-If the user is within 300m of the destination, then they are allowed to record their check in.
+If the user is within 30m of the destination, then they are allowed to record their check in.
 
 Users are able to add in new destinations by putting in the address. Using the geocoder gem,
 it converts the address to longitude and latitude.
--- app/assets/javascripts/geocoder.js
+++ app/assets/javascripts/geocoder.js
@@ -71,14 +71,14 @@
                         //     // if unable to find their location, then alert them
                         //     window.alert('Geocoder failed due to: ' + status);
                         // }
-                        // if the user is more than 300m away from the destination, disable the check_in_button
-                        if(distance <= 300){
+                        // if the user is more than 30m away from the destination, disable the check_in_button
+                        if(distance <= 30){
                             
                             // list how far away user is
-                            distance_output = "You are within 300m of " + destination;
+                            distance_output = "You are within 30m of " + destination;
                         }
                         else{
-                            // if user is more than 300m away from the destination, then disable to check_in_button
+                            // if user is more than 30m away from the destination, then disable to check_in_button
                             disable("check_in_button");
                             distance_output = "You are "+ distance + "m away from " + destination + ". You need to be within 300m.";
                         }
