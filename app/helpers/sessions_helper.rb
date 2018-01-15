007618aacd8c792160341b90a323ef89a061d317
Lena Zheng <lzhengem@gmail.com>
2018-01-15 06:25:21 +0000

    refractor current_user code

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,11 +6,11 @@ module SessionsHelper
     # returns current user if they are logged in
     def current_user
         # if there is a current session, use the id from there. else, use the cookie user id
-        if session[:user_id]
+        if (user_id = session[:user_id])
             # if @current user is not set, find it with the session id, otherwise return the @current_user
-            @current_user ||= User.find_by(id: session[:user_id])
-        elsif cookies.signed[:user_id]
-            user = User.find_by(id: cookies.signed[:user_id])
+            @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.signed[:user_id])
+            user = User.find_by(id: user_id)
             if user && user.authenticated?(cookies[:remember_token])
                 log_in user
                 @current_user = user
