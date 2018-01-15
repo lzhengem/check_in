37167266c614653a1b7055b9790e83df97bc496f
Lena Zheng <lzhengem@gmail.com>
2018-01-15 00:50:38 +0000

    add password to user, usng has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115002436) do
+ActiveRecord::Schema.define(version: 20180115003209) do
 
   create_table "check_ins", force: :cascade do |t|
     t.string   "location"
@@ -49,8 +49,9 @@ ActiveRecord::Schema.define(version: 20180115002436) do
     t.string   "name"
     t.string   "email"
     t.boolean  "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
