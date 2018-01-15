cb6b9ffdd046ec8bd28a6fd7e04ce61a1d0061b0
Lena Zheng <lzhengem@gmail.com>
2018-01-15 05:24:52 +0000

    can see logout if logged in, and login if not logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,8 +3,14 @@ module SessionsHelper
         session[:user_id] = user.id
     end
     
+    # returns current user if they are logged in
     def current_user
         # if @current user is not set, find it with the session id, otherwise return the @current_user
         @current_user ||= User.find_by(id: session[:user_id])
+    end
+    
+    # check to see if the user is logged in
+    def logged_in?
+        !current_user.nil?
     end
 end
