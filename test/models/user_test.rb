ee4c6254996c14ebfc783d07c6ef7f38f4bb505a
Lena Zheng <lzhengem@gmail.com>
2018-01-15 00:28:33 +0000

    added test for lower case email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -60,4 +60,11 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
   
+  test "email addresses should be saved as lower case" do
+    mixed_case_email = "Foo@EXamPlE.Com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase @user.reload.email
+  end
+  
 end
