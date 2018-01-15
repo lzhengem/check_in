a92e9a192963a36fb614af183c249c2cf5d0b0df
Lena Zheng <lzhengem@gmail.com>
2018-01-15 04:59:53 +0000

    tell the user wrong email/pw combo only once

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     else
-      flash[:danger] = 'Invalid email/password combination'
+      # flash.now will disappear as soon as there is an additional request
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
