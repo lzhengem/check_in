b3c3bb30124945d9c03a724660d7fadd4afc1b22
Lena Zheng <lzhengem@gmail.com>
2018-01-15 00:57:24 +0000

    user password must be present

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
                         length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}
     # this enforces validations on the virtual password and password_confirmation
     has_secure_password
-    validates :password, length: {minimum: 6}
+    validates :password, presence: true, length: {minimum: 6}
 end
