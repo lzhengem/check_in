2ed169ed6b5a29a6013ed780a38cd315643f34c8
Lena Zheng <lzhengem@gmail.com>
2018-01-15 05:04:26 +0000

    allow session to remember user id when they log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user
     else
       # flash.now will disappear as soon as there is an additional request
       flash.now[:danger] = 'Invalid email/password combination'
