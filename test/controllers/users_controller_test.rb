a0c89ebf079a3ee8d01c21f031e0a414d2c163ad
Lena Zheng <lzhengem@gmail.com>
2018-01-15 01:23:02 +0000

    added signup for new users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,8 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "signup path should get new" do
+    get signup_path
+    assert_response :success
+  end
 end
