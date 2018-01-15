09e22aea83356545817b940c748e3333c2d955fa
Lena Zheng <lzhengem@gmail.com>
2018-01-15 06:30:54 +0000

    remove duplicate log_out methdos

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,10 +43,4 @@ module SessionsHelper
         cookies.delete(:user_id)
         cookies.delete(:remember_token)
     end
-    
-    def log_out
-        forget(current_user)
-        session.delete(:user_id)
-        @current_user = nil
-    end
 end
