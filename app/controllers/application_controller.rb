14e7cd16786b89b3ca5ac04e3d5a120d5403a591
Lena Zheng <lzhengem@gmail.com>
2018-01-15 05:02:04 +0000

    include sessionshelper in applicationcontroller so it will be available in controllers as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
+  
   def remote_ip
     if request.remote_ip == '127.0.0.1'
       # Hard coded remote address
